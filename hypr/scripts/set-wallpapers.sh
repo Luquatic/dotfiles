8a45dec331bd25d27c0d05aef7bae8b4ee7abc82
Jessey <jesseyfransen@gmail.com>
2025-08-21 09:26:09 +0200

    initial commit

---
+++ hypr/scripts/set-wallpapers.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+swww-daemon &
+sleep 10
+swww img -o DP-1 ~/Pictures/1-scenery-pink-lakeside-sunset-lake-landscape-scenic-panorama-7680x3215-144.png
+swww img -o DP-3 ~/Pictures/witcher-wolf-medallion-left.jpg
