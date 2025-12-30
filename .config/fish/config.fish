5651ab4c7871c71f8c37015bf25916b7485cbbb9
Jessey Fransen <jesseyfransen@gmail.com>
2025-12-30 11:17:11 +0100

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,10 +5,26 @@ end
 starship init fish | source
 
 set fish_greeting
+set fish_color_error
+
+export BAT_THEME="Catppuccin Mocha"
+export FZF_DEFAULT_COMMAND="fd --type f --hidden --exclude .git"
+export FZF_DEFAULT_OPTS='--bind "enter:become(nvim {})"'
 
 alias ls "eza --icons"
 alias lsa "eza -lha --group-directories-first --icons=auto"
-alias hyprc "nvim ~/.config/hypr"
-alias audioc "omarchy-launch-or-focus-tui wiremix"
+alias lt "eza --tree --level=2 --long --icons --git"
+alias lta "lt -a"
+
+alias ff "fzf --preview 'bat --style=numbers --color=always {}'"
 alias clip "xclip -selection clipboard"
-alias wms "~/Development/WMS.jessey.fransen"
+
+# Directories
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias wms "$HOME/Development/WMS.jessey.fransen"
+
+# Config
+alias hyprc "nvim $HOME/.config/hypr"
+alias audioc "omarchy-launch-or-focus-tui wiremix"
